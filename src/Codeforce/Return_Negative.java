3626dc5715fd4a8c342ce80bdb9c6c69dcdca21f
RitaCJ <ritajoao0121@gmail.com>
2025-08-10 14:45:11 +0100

    Earth

---
+++ src/Codeforce/Return_Negative.java
@@ -0,0 +1,32 @@
+package Codeforce;
+
+import java.io.*;
+
+public class Return_Negative {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        final int num = Integer.parseInt(br.readLine());
+        int result;
+
+        result = makeNegative(num);
+        pw.println(result);
+        pw.flush();
+    }
+
+    public static int makeNegative(final int x) {
+       int num_x;
+
+       if(x <= 0){
+           return x;
+       }
+
+       num_x = 2 * x;
+       num_x = x - num_x;
+
+        return num_x; // Your code here.
+
+    }
+}
